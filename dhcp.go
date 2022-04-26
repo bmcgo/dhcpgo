8e8784e630deb20b4feb2202f8689ac723877260
redixin <redixin@users.noreply.github.com>
2022-04-26 23:55:16 +0300

    Add FindFreeIP

--- dhcp.go
+++ dhcp.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"net"
 	"strconv"
 	"strings"
 	"time"
@@ -29,30 +29,30 @@ func ParseIPv4(s string) (IPv4, error) {
 	if err != nil {
 		return 0, err
 	}
-	ip = ip | 0xff0000 & uint32(n<<16)
+	ip = ip | 0xff0000&uint32(n<<16)
 
 	n, err = strconv.ParseUint(bits[2], 10, 8)
 	if err != nil {
 		return 0, err
 	}
-	ip = ip | 0xff00 & uint32(n<<8)
+	ip = ip | 0xff00&uint32(n<<8)
 
 	n, err = strconv.ParseUint(bits[3], 10, 8)
 	if err != nil {
 		return 0, err
 	}
-	ip = ip | 0xff & uint32(n)
+	ip = ip | 0xff&uint32(n)
 
 	return IPv4(ip), nil
 }
 
-func (i *IPv4) String() string {
-	ip := uint32(*i)
+func (i IPv4) String() string {
+	ip := uint32(i)
 	return fmt.Sprintf("%d.%d.%d.%d",
-		ip >> 24 & 0xff,
-		ip >> 16 & 0xff,
-		ip >> 8 & 0xff,
-		ip & 0xff)
+		ip>>24&0xff,
+		ip>>16&0xff,
+		ip>>8&0xff,
+		ip&0xff)
 }
 
 func (i *IPv4) Inc() {
@@ -60,7 +60,7 @@ func (i *IPv4) Inc() {
 }
 
 func (i *IPv4) Next() *IPv4 {
-	ip := *i+1
+	ip := *i + 1
 	return &ip
 }
 
@@ -71,32 +71,83 @@ type Lease struct {
 }
 
 type Range struct {
-	IPFrom net.IP
-	IPTo net.IP
-	lastIP net.IP
-	leaseCache map[string]Lease
+	IPFrom    IPv4
+	IPTo      IPv4
+	LeaseTime time.Duration
+
+	currentIP  IPv4
+	leaseCache map[string]*Lease
 }
 
-func NewRange(from string, to string) (*Range, error) {
+func NewRange(from string, to string, leaseTime time.Duration) (*Range, error) {
+	ipfrom, err := ParseIPv4(from)
+	if err != nil {
+		return nil, err
+	}
+	ipto, err := ParseIPv4(to)
+	if err != nil {
+		return nil, err
+	}
+	if from > to {
+		return nil, errors.New("from > to")
+	}
+
 	//TODO: validate IPFrom < IPTo
 	return &Range{
-		IPFrom: net.ParseIP(from),
-		IPTo: net.ParseIP(to),
-		lastIP: net.ParseIP(from),
-		leaseCache: make(map[string]Lease),
-	},
-	nil
+			IPFrom:     ipfrom,
+			IPTo:       ipto,
+			LeaseTime:  leaseTime,
+			leaseCache: make(map[string]*Lease),
+		},
+		nil
 }
 
-func (r *Range) GetFreeIP() *Lease {
-	//var lease Lease
-	var ok bool
-	r.lastIP[0] += 1
-	for {
-		//lease, ok = r.leaseCache[r.lastIP.String()]
-		if !ok {
+func (r *Range) incrementCurrentIP() {
+	r.currentIP.Inc()
+	if r.currentIP > r.IPTo {
+		r.currentIP = r.IPFrom
+	}
+}
 
+func (r *Range) FindFreeIP() *Lease {
+	if r.currentIP == 0 {
+		r.currentIP = r.IPFrom
+	} else {
+		r.incrementCurrentIP()
+	}
+	var lease *Lease
+	var oldestLease *Lease
+	var ok bool
+	expiredTime := time.Now().Add(-r.LeaseTime)
+	firstIp := r.currentIP
+	for {
+		lease, ok = r.leaseCache[r.currentIP.String()]
+		if !ok {
+			lease = &Lease{
+				IP:         r.currentIP.String(),
+				LastUpdate: time.Now(),
+			}
+			r.leaseCache[lease.IP] = lease
+			return lease
 		}
-		return nil
+		//TODO: check for ACK
+		if lease.LastUpdate.Before(expiredTime) {
+			if oldestLease == nil {
+				oldestLease = lease
+			} else {
+				if oldestLease.LastUpdate.After(lease.LastUpdate) {
+					oldestLease = lease
+				}
+			}
+		}
+		r.incrementCurrentIP()
+		if firstIp == r.currentIP {
+			if oldestLease != nil {
+				oldestLease.Ack = false
+				return oldestLease
+			} else {
+				return nil
+			}
+		}
 	}
 }
--- dhcp_test.go
+++ dhcp_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"log"
 	"testing"
+	"time"
 )
 
 func assertTrue(t *testing.T, b bool) {
@@ -10,11 +12,14 @@ func assertTrue(t *testing.T, b bool) {
 	}
 }
 
-func TestIPv4_Parse_Next_Inc(t *testing.T) {
-	i := IPv4(1)
-	assertTrue(t, "0.0.0.1" == i.String())
-	i.Inc()
-	assertTrue(t, "0.0.0.2" == i.String())
+func assertEqual(t *testing.T, expected interface{}, actual interface{}) {
+	if expected != actual {
+		log.Printf("%v != %v", expected, actual)
+		t.Fail()
+	}
+}
+
+func TestIPv4_Parse_String_Next_Inc(t *testing.T) {
 	i, err := ParseIPv4("1.2.3.254")
 	assertTrue(t, err == nil)
 	assertTrue(t, "1.2.3.254" == i.String())
@@ -23,7 +28,23 @@ func TestIPv4_Parse_Next_Inc(t *testing.T) {
 	i.Inc()
 	assertTrue(t, "1.2.4.0" == i.String())
 	assertTrue(t, "1.2.4.1" == i.Next().String())
-
 	i, err = ParseIPv4("1.2.3.300")
 	assertTrue(t, err != nil)
+}
+
+func TestNewRange_FindFreeIP(t *testing.T) {
+	r, err := NewRange("10.1.1.1", "10.1.1.3", time.Second * 5)
+	assertTrue(t, err == nil)
+
+	l1 := r.FindFreeIP()
+	assertEqual(t, l1.IP, "10.1.1.1")
+
+	l2 := r.FindFreeIP()
+	assertEqual(t, l2.IP,  "10.1.1.2")
+
+	l3 := r.FindFreeIP()
+	assertEqual(t, l3.IP, "10.1.1.3")
+
+	l4 := r.FindFreeIP()
+	assertTrue(t, l4 == nil)
 }
